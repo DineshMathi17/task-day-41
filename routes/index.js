835b0d45dcffc7b3a89e9e203bf83dca1248b728
DineshMathi17 <121874583+DineshMathi17@users.noreply.github.com>
2023-06-13 16:03:24 +0530

    Update index.js

--- routes/index.js
+++ routes/index.js
@@ -4,7 +4,7 @@ var router = express.Router();
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.status(200).send(
-    {message:"Mentor and Student Assigning with Database."}
+    {message:"Mentor and Student Assigning with Database"}
 );
 });
 
